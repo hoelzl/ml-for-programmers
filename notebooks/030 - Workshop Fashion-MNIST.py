ecbc0da9c35b1721fcf189d3e3f0405ebaaf0235
Matthias Hölzl <tc@xantira.com>
2022-11-11 02:09:40 +0100

    Add linked notebooks in percent format

---
+++ notebooks/030 - Workshop Fashion-MNIST.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# <h1 style="text-align:center;">Machine Learning for Programmers</h1>
+# <h2 style="text-align:center;">Workshop Fashion-MNIST</h2>
+# <h3 style="text-align:center;">Dr. Matthias Hölzl</h3>
+#
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ### Workshop: Fashion MNIST
+#
+# - Get data: `fetch_openml(data_id=40996)`
+# - Develop a classifier that can recognize images of trousers
+
+# %% slideshow={"slide_type": "slide"}
---
+++ notebooks/078a - Workshop Salaries.py
@@ -0,0 +1,65 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # 078a - Workshop: Salaries
+#
+# The module `ml_for_programmers.data.generators.fake_salary` contains a number of synthetic datasets
+# that represent salary as a function of ages and education level, or ages and profession.
+#
+# Analyze how `linear_salaries`, `stepwise_salaries`, `interpolated_salaries` and `multivar_salaries` depend on `ages` and `education_levels` and train regression models (at least linear and decision tree models) that model these dependencies.
+#
+# Do the same for `multidist_ages`, `professions`, and `multidist_salaries`.
+#
+# *Hint:* The `fake_salary` module contains a number of plots that show the relatinships; to display them run the file as main module or interactively in VS Code. Please try to solve the exercises yourself before looking at the plots.
+
+# %%
+import matplotlib.pyplot as plt
+
+# %%
+from ml_for_programmers.data.generators.fake_salary import (
+    ages,
+    education_levels,
+    
+    linear_salaries, 
+    stepwise_salaries, 
+    interpolated_salaries, 
+    multivar_salaries,
+    
+    multidist_ages,
+    professions,
+    
+    multidist_salaries,
+)
+
+# %%
+ages.shape, education_levels.shape
+
+# %%
+(linear_salaries.shape,
+ stepwise_salaries.shape,
+ interpolated_salaries.shape,
+ multivar_salaries.shape)
+
+# %%
+plt.scatter(ages[:500], linear_salaries[:500], alpha=0.25);
+
+# %%
+multidist_ages.shape, professions.shape, multidist_salaries.shape
+
+# %%
+plt.scatter(multidist_ages, multidist_salaries, alpha=0.25);
+
+# %%
